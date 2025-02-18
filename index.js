27a44b6cfddc9ba34484c30c747bf98dd35cab4a
Rakib <rakibuzzaman1177@gmail.com>
2025-02-18 08:32:15 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById('button').addEventListener('click', ()=>{
+    document.getElementById('plant').classList.remove('hide');
+})
+
+
+document.getElementById('reButton').addEventListener('click', ()=>{
+    document.getElementById('plant').classList.add('hide');
+})
